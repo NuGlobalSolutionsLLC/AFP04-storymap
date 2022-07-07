d1adc41d137507e290b7c5e49547fa159262c791
Marc Compte <marc@braquets.cat>
2022-07-07 20:01:27 +0200

    Add legend to the Layer List items.

--- src/pages/MainMap.vue
+++ src/pages/MainMap.vue
@@ -9,18 +9,24 @@
         :max-zoom="19"
         :options="mapOptions"
         >
+
       <l-control-attribution
           position="bottomright"
           :prefix="attributionPrefix"
           />
+
       <l-tile-layer
           url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
           attribution="AFP4 WebGIS"
           :maxZoom="22"
           />
+
       <l-geo-json v-for="geojson, index in geoJsons" :key="index"
           :geojson="geojson" :options="geojson.options"
           />
+
+      <div class="legends" v-html="legends"></div>
+
     </l-map>
 
   </q-page>
@@ -44,17 +50,17 @@ export default defineComponent({
   setup () {
     const pageRef = ref(null)
     const mapRef = ref(null)
+    const mapOptions = {
+      preferCanvas: true,
+      attributionControl: false
+    }
+    const $store = useMapStore()
     let geojsonOptions = ref({})
     let height = ref(0)
     let circle
     let template
-    const $store = useMapStore()
-    let map = null
+    let map
     let leftDrawerOpen = true
-    const mapOptions = {
-      // preferCanvas: true,
-      attributionControl: false
-    }
 
     const getActiveGroups = () => {
       return $store.layers.map(group => {
@@ -66,6 +72,12 @@ export default defineComponent({
         }
         return false
       }).filter(group => group)
+    }
+
+    const getActiveLayers = () => {
+      return getActiveGroups().reduce((previous, group) => {
+        return previous.concat(...group.childs)
+      }, [])
     }
 
     const getFeatureStyle = (feature, template) => {
@@ -83,9 +95,7 @@ export default defineComponent({
     }
 
     const geoJsons = computed(() => {
-      const layers = getActiveGroups().reduce((previous, group) => {
-        return previous.concat(...group.childs)
-      }, [])
+      const layers = getActiveLayers()
       const geojsons = layers.map(layer => {
         if (layer.data) {
           return Object.assign({}, layer.data)
@@ -102,7 +112,6 @@ export default defineComponent({
               onEachFeature: function(feature, leafletLayer) {
                 let popup
                 leafletLayer.on('mouseover', function (event) {
-                  // console.log(event.target)
                   this.setStyle(event.target.options.hoverStyle)
                   event.target.bringToFront()
                   popup = L.popup()
@@ -114,10 +123,8 @@ export default defineComponent({
                   this.setStyle(getFeatureStyle(feature, layer.template))
                   popup.close()
                 })
-                // leafletLayer.bindTooltip(layer.template.tooltip)
                 leafletLayer.on('click', function () {
-                  // Let's say you've got a property called url in your geojsonfeature:
-                  window.location = feature.properties.url
+
                 })
               }
             }
@@ -125,6 +132,23 @@ export default defineComponent({
         }
       })
       return geojsons
+    })
+
+    const legends = computed(() => {
+      const layers = getActiveLayers()
+      const html = layers.map(layer => {
+        const categories = layer.template.limits.map((limit, index) => {
+          return `<li><span class="sample" style="background: ${layer.template.colors[index]};">&nbsp;</span> < ${limit}</li>`
+        })
+        categories.push(`
+          <li>
+            <span class="sample" style="background: ${layer.template.colors[layer.template.colors.length - 1]};">&nbsp;</span>
+            >= ${layer.template.limits[layer.template.limits.length - 1]}
+          </li>
+        `)
+        return `<ul class="legend">${categories.join("")}</ul>`
+      })
+      return html
     })
 
     const getGeoJsons = async () => {
@@ -187,6 +211,7 @@ export default defineComponent({
       geoJsons,
       geojsonOptions,
       height,
+      legends,
       mapOptions,
       mapRef,
       pageRef
@@ -202,5 +227,33 @@ export default defineComponent({
   color: white;
   padding: 20px;
   z-index: 99999;
+}
+.legends {
+  position: absolute;
+  z-index: 400;
+  right: 5px;
+  top: 5px;
+}
+:deep(.legends) .legend {
+  background: white;
+  border-radius: 8px;
+  margin: 5px;
+  padding: 10px;
+}
+:deep(.legends) ul {
+  list-style: none;
+}
+:deep(.legends) ul li {
+  display: flex;
+  align-items: center;
+  font-size: 1.2em;
+}
+:deep(.legends) span {
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  border: 1px solid black;
+  border-radius: 50%;
+  margin-right: 10px;
 }
 </style>
