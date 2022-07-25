de0dc6465311d032e32954b9accc375d72f20448
Marc Compte <marc@braquets.cat>
2022-07-25 19:29:20 +0200

    Style scrollbar.

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,29 @@ export default defineComponent({
 })
 </script>
 
-<style>
+<style lang="scss">
 #q-app, html, body {
   overflow: hidden;
+}
+/* width */
+::-webkit-scrollbar {
+  width: 6px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #fff;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: $primary;
+  // background: lighten($primary, 10%);
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  // background: $primary;
+  background: darken($primary, 10%);
 }
 </style>
